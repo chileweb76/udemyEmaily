687c48c9a4ffdcdf6b3a08d946df194842f99d18
“Christopher <christopherhileweb@gmail.com>
2023-11-04 18:18:11 -0400

    scope

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,12 @@ module.exports = (app) => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
 
   app.get("/api/logout", (req, res) => {
     req.logout();
